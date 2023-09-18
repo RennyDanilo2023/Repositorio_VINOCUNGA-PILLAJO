350d3dc25db74b926c3068c82db3b9513748e0b8
RennyDanilo2023 <142845224+RennyDanilo2023@users.noreply.github.com>
2023-09-17 19:44:50 -0500

    (Semana 13) Tarea: Definición y uso de funciones en programación

--- Sem_13.TemperaturasCuidades.py
+++ Sem_13.TemperaturasCuidades.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Crear una matriz 3D para almacenar datos de temperaturas
-# Primera dimensión: Ciudades (3 ciudades)
-# Segunda dimensión: Días de la semana (7 días)
-# Tercera dimensión: Semanas (4 semanas)
+# Crear matriz de datos
 temperaturas = [
-    [   # Ciudad 1
-        [   # Semana 1
+    [  # Ciudad 1
+        [  # Semana 1
             {"day": "Lunes", "temp": 78},
             {"day": "Martes", "temp": 80},
             {"day": "Miércoles", "temp": 82},
@@ -15,7 +12,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 88},
             {"day": "Domingo", "temp": 92}
         ],
-        [   # Semana 2
+        [  # Semana 2
             {"day": "Lunes", "temp": 76},
             {"day": "Martes", "temp": 79},
             {"day": "Miércoles", "temp": 83},
@@ -24,7 +21,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 89},
             {"day": "Domingo", "temp": 93}
         ],
-        [   # Semana 3
+        [  # Semana 3
             {"day": "Lunes", "temp": 77},
             {"day": "Martes", "temp": 81},
             {"day": "Miércoles", "temp": 85},
@@ -33,7 +30,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 91},
             {"day": "Domingo", "temp": 95}
         ],
-        [   # Semana 4
+        [  # Semana 4
             {"day": "Lunes", "temp": 75},
             {"day": "Martes", "temp": 78},
             {"day": "Miércoles", "temp": 80},
@@ -43,8 +40,8 @@ temperaturas = [
             {"day": "Domingo", "temp": 91}
         ]
     ],
-    [   # Ciudad 2
-        [   # Semana 1
+    [  # Ciudad 2
+        [  # Semana 1
             {"day": "Lunes", "temp": 72},
             {"day": "Martes", "temp": 74},
             {"day": "Miércoles", "temp": 76},
@@ -53,7 +50,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 80},
             {"day": "Domingo", "temp": 85}
         ],
-        [   # Semana 2
+        [  # Semana 2
             {"day": "Lunes", "temp": 74},
             {"day": "Martes", "temp": 76},
             {"day": "Miércoles", "temp": 78},
@@ -62,7 +59,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 82},
             {"day": "Domingo", "temp": 87}
         ],
-        [   # Semana 3
+        [  # Semana 3
             {"day": "Lunes", "temp": 75},
             {"day": "Martes", "temp": 77},
             {"day": "Miércoles", "temp": 79},
@@ -71,7 +68,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 83},
             {"day": "Domingo", "temp": 88}
         ],
-        [   # Semana 4
+        [  # Semana 4
             {"day": "Lunes", "temp": 73},
             {"day": "Martes", "temp": 75},
             {"day": "Miércoles", "temp": 77},
@@ -81,8 +78,8 @@ temperaturas = [
             {"day": "Domingo", "temp": 86}
         ]
     ],
-    [   # Ciudad 3
-        [   # Semana 1
+    [  # Ciudad 3
+        [  # Semana 1
             {"day": "Lunes", "temp": 84},
             {"day": "Martes", "temp": 87},
             {"day": "Miércoles", "temp": 88},
@@ -91,7 +88,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 79},
             {"day": "Domingo", "temp": 76}
         ],
-        [   # Semana 2
+        [  # Semana 2
             {"day": "Lunes", "temp": 83},
             {"day": "Martes", "temp": 86},
             {"day": "Miércoles", "temp": 87},
@@ -100,7 +97,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 78},
             {"day": "Domingo", "temp": 75}
         ],
-        [   # Semana 3
+        [  # Semana 3
             {"day": "Lunes", "temp": 85},
             {"day": "Martes", "temp": 88},
             {"day": "Miércoles", "temp": 89},
@@ -109,7 +106,7 @@ temperaturas = [
             {"day": "Sábado", "temp": 80},
             {"day": "Domingo", "temp": 77}
         ],
-        [   # Semana 4
+        [  # Semana 4
             {"day": "Lunes", "temp": 82},
             {"day": "Martes", "temp": 85},
             {"day": "Miércoles", "temp": 86},
@@ -121,11 +118,25 @@ temperaturas = [
     ]
 ]
 
-# Calcular el promedio de temperaturas para cada ciudad y semana
-for ciudad_index, ciudad in enumerate(temperaturas):
-    for semana_index, semana in enumerate(ciudad):
+# Calcular promedios por semana
+promedios_por_semana = []
+for ciudad in temperaturas:
+    promedios_ciudad = []
+    for semana in ciudad:
         suma_temperaturas = 0
         for dia in semana:
             suma_temperaturas += dia["temp"]
         promedio_semana = suma_temperaturas / len(semana)
-        print("Promedio de temperaturas en Ciudad {}, Semana {}: {:.2f}°C".format(ciudad_index + 1, semana_index + 1, promedio_semana))
+        promedios_ciudad.append(promedio_semana)
+
+    promedios_por_semana.append(promedios_ciudad)
+
+# Calcular promedios totales por ciudad
+for i, promedios_ciudad in enumerate(promedios_por_semana):
+    suma = 0
+    for promedio_semana in promedios_ciudad:
+        suma += promedio_semana
+    promedio_total = suma / len(promedios_ciudad)
+
+    print("Promedio ciudad {}: {}".format(i + 1, promedio_total))
+
